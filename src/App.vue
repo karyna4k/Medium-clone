a86fa3b174848b81b266a75e8a8f578ec36ee756
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-10-05 16:30:24 +0300

    feat: add Register page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/register">Sign in</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/Register.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign Up</h1>
+          <p class="text-xs-center">
+            <router-link to="{name: 'login}">Need an account?</router-link>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'McvRegister'
+}
+</script>
